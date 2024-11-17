be7462d84aca7e28df7b9e1f6691d6e9f7a89529
Manu88x <emmanuelokoth112@gmail.com>
2024-11-17 17:37:13 +0300

    code

---
+++ src/components/blog.js
@@ -0,0 +1,13 @@
+const blogData = {
+    name: "My Awesome Blog",
+    image: "https://example.com/blog-image.jpg",
+    about: "This is a blog about various interesting topics. Stay tuned for exciting posts!",
+    posts: [
+      { title: "Post 1", date: "2024-01-01" },
+      { title: "Post 2", date: "2024-01-05" },
+      { title: "Post 3", date: "2024-01-10" },
+    ],
+  };
+  
+  export default blogData;
+  
